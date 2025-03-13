e247994f7b4154636be6e9ce3dd7e7b3afae9d4d
jabareenlidor259 <jabareenlidor259@gmail.com>
2025-03-13 10:44:42 +0000

    Add 8.rs

---
+++ src/8.rs
@@ -0,0 +1,8 @@
+// Rust code for a school project
+
+fn main() {
+    let mut num1 = 5;
+    let mut num2 = 10;
+    let result = num1 + num2;
+    println!("The sum of {} and {} is {}", num1, num2, result);
+}
