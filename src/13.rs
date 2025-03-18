c8894761b7985c69af418013f25829b50fbcb33d
jabareenlidor259 <jabareenlidor259@gmail.com>
2025-03-18 01:27:07 +0000

    Add 13.rs

---
+++ src/13.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut numbers = vec![1, 2, 3, 4, 5];
+    
+    numbers.sort(); // Sort the vector in ascending order
+    
+    println!("{:?}", numbers); // Print the sorted vector
+}
